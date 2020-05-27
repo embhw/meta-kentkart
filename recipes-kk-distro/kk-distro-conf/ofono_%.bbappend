38dc11ba107d18718330f0d3c8b5fcd54e05ab18
embhw <embhw@kentkart.com.tr>
2020-05-27 16:05:06 +0300

    ofonod disabled,ppp&wvdial added

---
+++ recipes-kk-distro/kk-distro-conf/ofono_%.bbappend
@@ -0,0 +1,3 @@
+inherit systemd
+SYSTEMD_SERVICE_${PN} = "ofono.service"
+SYSTEMD_AUTO_ENABLE_${PN} = "disable"
--- setup-services.sh
+++ setup-services.sh
@@ -7,3 +7,6 @@
 echo "#SSH Enable" >> $BUILD_DIR/conf/local.conf
 echo "IMAGE_FEATURES += \"ssh-server-openssh\"" >> $BUILD_DIR/conf/local.conf
 
+echo "#PPP and WVDIAL adding" >> $BUILD_DIR/conf/local.conf
+echo "IMAGE_INSTALL_append += \" ppp\"" >> $BUILD_DIR/conf/local.conf
+echo "IMAGE_INSTALL_append += \" wvdial\"" >> $BUILD_DIR/conf/local.conf
